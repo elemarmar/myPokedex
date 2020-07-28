7a30703a5f74cc7b771ef2642043f643f1398eb6
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-28 09:12:46 +0200

    Add dynamic PokeType

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -3,14 +3,18 @@ import React from 'react';
 
 const Pokemon = (props) => {
 
+    // dynamic types
+    let pokeTypes = props.types.map(type => {
+        return <li key={type}>{type}</li>
+    })
+
     return (
         <div className="pokemon-card">
             <h3>{props.name}</h3>
             <span>{props.id}</span>
             <div className="pokemon-body">
                 <ul className="pokemon-types">
-                    <li>Fire</li>
-                    <li>Water</li>
+                    {pokeTypes}
                 </ul>
                 <div className="pokemon-img-container">
                     <img className="pokemon-img" src={props.picURL} alt="Pokemon name" />
