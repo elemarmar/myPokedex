fbd42af22cdbcc93154448d613e7e1d3cef650ad
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-28 09:51:28 +0200

    Add more styling

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -6,15 +6,15 @@ const Pokemon = (props) => {
 
     // dynamic types
     const pokeTypes = props.types.map(type => {
-        return <li key={type}>{type}</li>
+        return <li className="poke-type" key={type}>{type}</li>
     })
 
     return (
-        <div className="pokemon-card">
+        <div className={`pokemon-card ${props.types[0]}`}>
             <h3 className="poke-name">{props.name}</h3>
             <span className="poke-id">{props.id}</span>
             <div className="pokemon-body">
-                <ul className="pokemon-types">
+                <ul className={"pokemon-types"}>
                     {pokeTypes}
                 </ul>
                 <div className="pokemon-img-container">
